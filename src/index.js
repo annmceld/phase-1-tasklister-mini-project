2c4985037e3afb42d2bbc48f6b42e3f42f31c780
Ann McElderry <thayerann1@gmail.com>
2023-03-23 15:49:58 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+function addTask(e) {
+  let li = document.createElement('li');
+  let taskList = document.getElementById('tasks');
+  taskList.appendChild(li);
+  li.textContent = e.target["new-task-description"].value;
+  e.preventDefault();
+}
+
+function addToList() {
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', addTask)
+}
+
+addToList()
